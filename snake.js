4d308998f683873e310bb5891b1d24330ec092ad
Michael Fleming <flemingatx@gmail.com>
2017-01-14 01:51:08 -0600

    adds snake moves

--- snake.js
+++ snake.js
@@ -1,4 +1,5 @@
-const board = document.getElementById('board').children;
+const board = Array.from(document.getElementById('board').children)
+  .map(c => Array.from(c.children));
 const directions = {
   ArrowUp: 'north',
   ArrowRight: 'east',
@@ -8,13 +9,24 @@ const directions = {
   d: 'east',
   s: 'south',
   a: 'west',
-}
-document.onkeydown = (e) => { console.log(directions[e.key]) }
+};
+const snake = {
+  bearing: 'east',
+  x: 9,
+  y: 9,
+  history: [],
+};
+
+document.onkeydown = (e) => {
+  snake.bearing = directions[e.key];
+};
 
 function moveSnake() {
-  for (const i of board) {
-    console.log(i);
-  }
+  if (snake.bearing === 'north') snake.y += 1;
+  if (snake.bearing === 'east') snake.x += 1;
+  if (snake.bearing === 'south') snake.y -= 1;
+  if (snake.bearing === 'west') snake.x -= 1;
+  board[snake.y][snake.x].classList = 'snake';
 }
 
-// setInterval(moveSnake, 1000);
+setInterval(moveSnake, 1000);
