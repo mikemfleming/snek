efebafa826b4619b3b4e40a2fe60e300fdd7d35b
Michael Fleming <flemingatx@gmail.com>
2017-01-14 01:18:25 -0600

    first commit

--- snake.js
+++ snake.js
@@ -1,1 +1,20 @@
-console.log('snake')
+const board = document.getElementById('board').children;
+const directions = {
+  ArrowUp: 'north',
+  ArrowRight: 'east',
+  ArrowDown: 'south',
+  ArrowLeft: 'west',
+  w: 'north',
+  d: 'east',
+  s: 'south',
+  a: 'west',
+}
+document.onkeydown = (e) => { console.log(directions[e.key]) }
+
+function moveSnake() {
+  for (const i of board) {
+    console.log(i);
+  }
+}
+
+// setInterval(moveSnake, 1000);
