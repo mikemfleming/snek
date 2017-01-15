a6403480c3704c368d334a506f2b02c9eef0dc52
Michael Fleming <flemingatx@gmail.com>
2017-01-15 14:11:31 -0600

    splits into separate files for better inversion of control

--- snake.js
+++ snake.js
@@ -1,35 +1,9 @@
-const board = Array.from(document.getElementById('board').children)
-  .map(c => Array.from(c.children));
-
-const directions = {
-  ArrowUp: 'north',
-  ArrowRight: 'east',
-  ArrowDown: 'south',
-  ArrowLeft: 'west',
-  w: 'north',
-  d: 'east',
-  s: 'south',
-  a: 'west',
-};
-
-const momentum = {
-  ArrowUp: 'south',
-  ArrowRight: 'west',
-  ArrowDown: 'north',
-  ArrowLeft: 'east',
-  w: 'south',
-  d: 'west',
-  s: 'north',
-  a: 'east',
-};
-
 class Snake {
   constructor() {
     this.bearing = 'east';
     this.spine = [{ x: 7, y: 9 },
                   { x: 8, y: 9 },
                   { x: 9, y: 9 }];
-    this.food = this.genFood();
   }
   head() {
     return this.spine[this.spine.length - 1];
@@ -41,52 +15,6 @@ class Snake {
     const { x, y } = this.spine[0];
     this.spine.unshift({ x, y });
   }
-  eat(food) {
-    this.grow();
-    board[food.y][food.x].classList = 'snake';
-    this.genFood();
-  }
-  genFood() {
-    const x = Math.floor(Math.random() * 19);
-    const y = Math.floor(Math.random() * 19);
-    board[y][x].classList = 'food';
-    this.food = { x, y };
-    return this.food;
-  }
 }
 
 const snake = new Snake();
-let allowMove = true;
-const interval = setInterval(moveSnake, 250);
-
-document.onkeydown = (e) => {
-  const turn = e.key;
-  if (snake.bearing !== momentum[turn] && allowMove) {
-    snake.bearing = directions[e.key];
-    allowMove = false;
-  }
-};
-
-function paintSnake(old) {
-  for (const [idx, vert] of snake.spine.entries()) {
-    const { x, y } = vert;
-    board[y][x].classList = 'snake';
-  }
-  board[old.y][old.x].classList = '';
-}
-
-function moveSnake() {
-  allowMove = true;
-  let { x, y } = snake.head();
-  if (x === snake.food.x && y === snake.food.y) snake.eat({ x, y });
-  if (snake.bearing === 'north') y--;
-  if (snake.bearing === 'east') ++x;
-  if (snake.bearing === 'south') y++;
-  if (snake.bearing === 'west') x--;
-  const old = snake.move(x, y);
-  if (x > 19 || y > 19 || x < 0 || y < 0) {
-    clearInterval(interval);
-  } else {
-    paintSnake(old);
-  }
-}
--- world.js
+++ world.js
@@ -1,10 +1,49 @@
-// WORLD
-// - knows it needs to generate food
-// - knows it needs collision detection
-// - knows it needs to paint snake
+class World {
+  constructor() {
+    this.score = 0;
+    this.food = this.genFood();
+  }
+  genFood() {
+    const x = Math.floor(Math.random() * 19);
+    const y = Math.floor(Math.random() * 19);
+    board[y][x].classList = 'food';
+    this.food = { x, y };
+    return this.food;
+  }
+  feed(food) {
+    snake.grow();
+    board[food.y][food.x].classList = 'snake';
+    this.genFood();
+    score += 10;
+    scoreDiv.innerHTML = score;
+  }
+  paintSnake(old) {
+    for (const [_, vert] of snake.spine.entries()) {
+      const { x, y } = vert;
+      board[y][x].classList = 'snake';
+    }
+    board[old.y][old.x].classList = '';
+  }
+  moveSnake() {
+    allowMove = true;
+    let { x, y } = snake.head();
+    const collision = snake.spine
+      .slice(0, snake.spine.length - 1)
+      .find(v => v.x === x && v.y === y);
+    if (collision) clearInterval(interval);
+    if (x === world.food.x && y === world.food.y) world.feed({ x, y });
+    if (snake.bearing === 'north') y--;
+    if (snake.bearing === 'east') ++x;
+    if (snake.bearing === 'south') y++;
+    if (snake.bearing === 'west') x--;
+    const old = snake.move(x, y);
+    if (x > 19 || y > 19 || x < 0 || y < 0) {
+      clearInterval(interval);
+    } else {
+      world.paintSnake(old);
+    }
+  }
+}
 
-// SNEK
-// - knows where its head is
-// - knows it needs to move forward
-// - knows it needs to turn
-// - knows it needs to eat and grow
+const world = new World();
+const interval = setInterval(world.moveSnake, 250);
